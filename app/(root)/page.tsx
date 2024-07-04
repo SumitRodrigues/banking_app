2c8232782639a9bd8e6bc85b19d5bf0dfa62f157
SumitRodrigues <sumitrod11@gmail.com>
2024-07-04 08:44:21 -0700

    create headerbox and individual layouts/pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,24 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
-const page = () => {
+const Home = () => {
+    const loggedIn = { firstName: 'Sumit'};
+
   return (
-    <div>page</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox 
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName || 'Guest'}
+                    subtext="Access and manage your account and transactions efficiently."
+
+                />
+            </header>
+        </div>
+    </section>
   )
 }
 
-export default page
+export default Home
