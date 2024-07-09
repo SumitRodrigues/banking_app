6b83d614d302c910664e4d8836cbec8964353c7a
SumitRodrigues <sumitrod11@gmail.com>
2024-07-08 20:30:08 -0700

    complete sidebars

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,4 +1,5 @@
 import { formatAmount } from '@/lib/utils'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -15,7 +16,42 @@ const BankCard = ({ account, userName, showBalance = true}: CreditCardProps) => 
                         {formatAmount(account.currentBalance)}
                     </p>
                 </div>
+                <article className='flex flex-col gap-2'>
+                    <div className='flex justify-between'>
+                        <h1 className='text-12 font-semibold text-white'>
+                            {userName}
+                        </h1>
+                        <h2 className='text-12 font-semibold text-white'>
+                            ●● / ●●
+                        </h2>
+                    </div>
+                    <p className='text-14 font-semibold tracking-[1.1px] text-white'>
+                        ●●●● ●●●● ●●●● <span className='text-16'>1234</span>
+                    </p>
+                </article>
             </div>
+            <div className='bank-card_icon'>
+                <Image 
+                    src="/icons/Paypass.svg"
+                    height={20}
+                    width={20}
+                    alt='pay'
+                />
+                <Image 
+                    src="/icons/mastercard.svg"
+                    height={45}
+                    width={32}
+                    alt="mastercard"
+                    className='ml-5'
+                />
+            </div>
+            <Image 
+                src="/icons/lines.png"
+                width={316}
+                height={190}
+                alt='lines'
+                className='absolute top-0 left-0'
+            />
         </Link>
     </div>
   )
