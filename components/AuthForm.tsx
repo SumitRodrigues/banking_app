54df992de7a0a8cea24bb37000b6b0642ea14f00
SumitRodrigues <sumitrod11@gmail.com>
2024-07-21 10:10:57 -0700

    dwolla implementation

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -23,6 +23,7 @@ import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions'
+import PlaidLink from './PlaidLink'
 
 const formSchema = z.object({
     email: z.string().email(),
@@ -101,11 +102,11 @@ const AuthForm = ({ type }: { type: string }) => {
                     </h1>
                 </div>
             </header>
-            {user ? (
+            {/* {user ? ( */}
                 <div className='flex flex-col gap-4'>
-                    {/* PlaidLink */}
+                    <PlaidLink user={user} variant="primary" />
                 </div>
-            ) : (
+            {/* ) : ( */}
                 <>
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -187,7 +188,7 @@ const AuthForm = ({ type }: { type: string }) => {
                     </footer>
 
                 </>
-            )}
+            {/* )} */}
         </section>
     )
 }
